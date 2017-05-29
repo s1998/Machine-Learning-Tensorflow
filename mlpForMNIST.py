bea34ae640508366c61ad0de508db6af8d84640d
s1998 <pihooji@gmail.com>
2017-05-29 12:06:11 +0530

    mlp, 1 hidden layer mnist 91.34%

--- mlpForMNIST.py
+++ mlpForMNIST.py
@@ -6,18 +6,18 @@ xData = tf.placeholder(tf.float32, [None, 784])
 yData = tf.placeholder(tf.float32, [None, 10])
 
 w1 = tf.Variable(tf.random_uniform((784, 10)))
-b1 = tf.Variable(tf.zeros(10))
+b1 = tf.Variable(tf.ones(10))
 
 w2 = tf.Variable(tf.random_uniform((10,10)))
-b2 = tf.Variable(tf.zeros(10))
+b2 = tf.Variable(tf.ones(10))
 
 batchSize = 500
-yPredicted = tf.nn.sigmoid(tf.matmul(tf.nn.sigmoid( tf.matmul(xData, w1) + b1), w2) + b2)
+yPredicted = tf.nn.softmax(tf.matmul(tf.nn.softmax( tf.matmul(xData, w1) + b1), w2) + b2)
 crossEntropyLoss = - tf.reduce_sum(yData * tf.log(yPredicted) + (1-yData)*tf.log(1-yPredicted)) / batchSize
 
 trainer = tf.train.GradientDescentOptimizer(0.01).minimize(crossEntropyLoss)
 init = tf.global_variables_initializer()
-epochs = 10000
+epochs = 1000000
 
 resultsList = list()
 
@@ -30,10 +30,15 @@ with tf.Session() as sess:
         if(i%100==0):
             crossEntropyLossRes = sess.run(crossEntropyLoss, feed_dict={xData: batch[0], yData: batch[1]})
             accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(yData, 1), tf.argmax(yPredicted,1)), "float"))
-            accuracyRes = sess.run(accuracy, feed_dict={xData: batch[0], yData: batch[1]})
+            accuracyRes = sess.run(accuracy, feed_dict={xData: mnist.test.images, yData: mnist.test.labels})
             resultsList.append([i, crossEntropyLossRes, accuracyRes])
-            print(i, accuracyRes)
+            print("i%100 is zero here, accuracy, crossEntropyloss",i, accuracyRes, crossEntropyLossRes)
 
 for i in range(len(resultsList)):
     print(resultsList[i])
 
+"""
+Results :
+Iteration, accuracy, crossEntropyloss  :  445300 0.9134 0.443774
+
+"""
